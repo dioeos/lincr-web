3c1c703ffe9bcfbdd0263d731d77308ea852ccc2
ryandioneda <er.dioneda@gmail.com>
2026-03-22 16:03:56 -0400

    Changed dev start to watch code dir

--- server/dev-start.sh
+++ server/dev-start.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "Starting Falcon server..."
-io-watch . -- bundle exec falcon serve --bind http://0.0.0.0:9000 --config config.ru --restart
+io-watch lib -- bundle exec falcon serve --bind http://0.0.0.0:9000 --config config.ru --restart
