2dcc04673cf8a6f286213e255b0e808b3decbf07
ryandioneda <er.dioneda@gmail.com>
2026-04-18 23:25:49 -0400

    chore: Changed dev startup to check Ruby gems

--- server/dev-start.sh
+++ server/dev-start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Checking Ruby gems..."
+bundle check || bundle install
+
 echo "Starting Falcon server..."
 io-watch lib -- bundle exec falcon serve --bind http://0.0.0.0:9000 --config config.ru --restart
