383c374ab97aec407c47cbe9cd0ba2e745f8a80f
ryandioneda <er.dioneda@gmail.com>
2026-04-20 12:34:45 -0400

    fix: Changed post request tests to use stubbed service and janus client

---
+++ server/spec/requests/room_service/room_post_requests_spec.rb
@@ -0,0 +1,51 @@
+require_relative "../../spec_helper"
+
+RSpec.describe "Post", type: :request do
+  describe "POST /rooms/delete/:room_code" do
+    let(:fake_janus) do
+      instance_double(RoomService::Clients::JanusClient)
+    end
+
+    let(:destroy_service) do
+      RoomService::Container
+        .room_manager
+        .instance_variable_get(:@destroy_room_service)
+    end
+    
+    before do
+      @original_janus_client = destroy_service.instance_variable_get(:@janus_client)
+      destroy_service.instance_variable_set(:@janus_client, fake_janus)
+    end
+
+    after do
+    destroy_service.instance_variable_set(:@janus_client, @original_janus_client)
+    end
+
+    it "deletes the requested room" do
+      repo = RoomService::Repositories::RoomRepository.new
+      room = RoomService::Models::AppRoom.new(
+        room_code: "ABC1234",
+        janus_room_id: 6767
+      )
+      repo.save(room)
+
+      allow(fake_janus)
+        .to receive(:destroy_janus_room)
+        .with(6767)
+        .and_return(nil)
+
+      post "/api/v1/rooms/delete/ABC1234"
+      expect(last_response.status).to eq(200)
+
+      json = JSON.parse(last_response.body)
+      expect(json).to eq(
+        "message" => "Room ABC1234 deleted"
+      )
+
+      expect(fake_janus)
+        .to have_received(:destroy_janus_room)
+        .with(6767)
+    
+    end
+  end
+end
