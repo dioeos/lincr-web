d47290aea7ba91a5f2d2bc94826b1ce3e446adef
ryandioneda <er.dioneda@gmail.com>
2026-04-20 12:56:46 -0400

    fix: Inject repo into delete test for repo consistency

--- server/spec/requests/room_service/room_post_requests_spec.rb
+++ server/spec/requests/room_service/room_post_requests_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe "Post", type: :request do
       instance_double(RoomService::Clients::JanusClient)
     end
 
+    let(:repo) do
+    RoomService::Repositories::RoomRepository.new
+    end
+
     let(:destroy_service) do
       RoomService::Container
         .room_manager
@@ -14,15 +18,18 @@ RSpec.describe "Post", type: :request do
     
     before do
       @original_janus_client = destroy_service.instance_variable_get(:@janus_client)
+      @original_room_repo = destroy_service.instance_variable_get(:@room_repo)
+
       destroy_service.instance_variable_set(:@janus_client, fake_janus)
+      destroy_service.instance_variable_set(:@room_repo, repo)
     end
 
     after do
     destroy_service.instance_variable_set(:@janus_client, @original_janus_client)
+    destroy_service.instance_variable_set(:@repo, @repo)
     end
 
     it "deletes the requested room" do
-      repo = RoomService::Repositories::RoomRepository.new
       room = RoomService::Models::AppRoom.new(
         room_code: "ABC1234",
         janus_room_id: 6767
@@ -45,7 +52,15 @@ RSpec.describe "Post", type: :request do
       expect(fake_janus)
         .to have_received(:destroy_janus_room)
         .with(6767)
-    
+    end
+
+    it "returns 404 on RoomNotFoundError" do
+      post "/api/v1/rooms/delete/KOI1234"
+      expect(last_response.status).to eq(404)
+      json = JSON.parse(last_response.body)
+      expect(json).to eq(
+        "error" => "Room KOI1234 not found"
+      )
     end
   end
 end
